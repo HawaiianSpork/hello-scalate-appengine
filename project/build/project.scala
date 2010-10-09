6e801fe94d021f513866c0b439d15c0daf71179a
Yasushi Abe <yasushi.abe@gmail.com>
2010-10-10 04:32:15 +0900

    scalate 1.3

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class HelloScalateProject(info: ProjectInfo) extends AppengineProject(info) with net.stbbs.yasushi.ScalatePlugin {
-  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.2"
+  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.3"
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.6.1"
 
   //override def templateRoots = webappPath
