bde853e9dc6bd9ba71c8d57c319d30f6c425c157
Yasushi Abe <yasushi.abe@gmail.com>
2011-01-31 17:26:33 +0900

    update plugin version

--- project/build/project.scala
+++ project/build/project.scala
@@ -3,6 +3,7 @@ import sbt._
 class HelloScalateProject(info: ProjectInfo) extends AppengineProject(info) with net.stbbs.yasushi.ScalatePlugin {
   val scalate = "org.fusesource.scalate" % "scalate-core" % "1.3"
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.6.1"
+  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
 
   //override def templateRoots = webappPath
 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val appenginePlugin = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.1"
-  val scalatePlugin = "net.stbbs.yasushi" % "sbt-scalate-plugin" % "1.0"
+  val appenginePlugin = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.2"
+  val scalatePlugin = "net.stbbs.yasushi" % "sbt-scalate-plugin" % "1.1"
 }
